100165f09071d569adc4c457066b70eaa06521af
Paul Timmermann <paul@paul-timmermann.de>
2026-04-16 01:03:15 +0200

    Show all tabs on load, show funny page when all closed

--- public/script.js
+++ public/script.js
@@ -5,14 +5,6 @@ const tabs_selector = document.querySelector("#tabs");
 const currentPath = window.location.pathname;
 const isIndex = currentPath === '/' || currentPath.endsWith('/index.html') || currentPath === '/index';
 
-if (!isIndex) {
-	document.querySelector("#tab_1").style.display = "none";
-	document.querySelector("#x_1").style.display = "none";
-} else {
-	document.querySelector("#tab_2").style.display = "none";
-	document.querySelector("#x_2").style.display = "none";
-}
-
 const fold = () => {
 	if (nav_selector.style.backgroundColor === "black") {
 	nav_selector.style.backgroundColor = "#0e0e0e";
@@ -27,14 +19,6 @@ const fold = () => {
 }
 
 const closetab = (tabNumber) => {
-	const isCurrentPageTab = (tabNumber === 1 && isIndex) || (tabNumber === 2 && !isIndex);
-	
-	if (isCurrentPageTab) {
-		const targetPage = tabNumber === 1 ? '/whatelse' : '/';
-		window.location.href = targetPage;
-		return;
-	}
-	
 	document.querySelector("#tab_" + tabNumber).style.display = "none";
 	document.querySelector("#x_" + tabNumber).style.display = "none";
 	
@@ -51,25 +35,15 @@ const closetab = (tabNumber) => {
 				<a href="#" onclick="resetTabs(); return false;" style="color: #af1b3e; font-size: 12px;">get tabs back</a>
 			</div>
 		`;
+	} else {
+		const remainingTab = tab1Visible ? 1 : 2;
+		window.location.href = remainingTab === 1 ? '/' : '/whatelse';
 	}
 }
 
 const resetTabs = () => {
-	document.querySelector("#tab_1").style.display = "";
-	document.querySelector("#x_1").style.display = "";
-	document.querySelector("#tab_2").style.display = "";
-	document.querySelector("#x_2").style.display = "";
-	
 	document.querySelector("#tabs").innerHTML = `
 		<a id="tab_1" href="/">	<span><p>index.html</p></span></a>	<a id="x_1" onclick="closetab(1)">x</a>
 		<a id="tab_2" href="/whatelse">		<span><p>what_else.html</p></span></a>	<a id="x_2" onclick="closetab(2)">x</a>
 	`;
-	
-	if (isIndex) {
-		document.querySelector("#tab_1").style.display = "none";
-		document.querySelector("#x_1").style.display = "none";
-	} else {
-		document.querySelector("#tab_2").style.display = "none";
-		document.querySelector("#x_2").style.display = "none";
-	}
 }
